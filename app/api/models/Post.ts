00872431968e90c21dcd2836b263a5faf023f6e1
Aayushman-nvm <aayushman.jha001@gmail.com>
2025-09-15 22:42:57 +0530

    Posts model

---
+++ app/api/models/Post.ts
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const postSchema=new mongoose.Schema({
+    title:String,
+    description:String,
+    posterUrl:String,
+    status:String,
+});
+
+const Post= mongoose.model("Post", postSchema);
+export default Post;
