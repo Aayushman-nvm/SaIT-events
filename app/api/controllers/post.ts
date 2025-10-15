c27749b0e674f79cd85f8112629205fcf473685f
Aayushman-nvm <aayushman.jha001@gmail.com>
2025-10-15 23:44:25 +0530

    controllers updated

--- app/api/controllers/post.ts
+++ app/api/controllers/post.ts
@@ -5,23 +5,28 @@ import { NextResponse } from 'next/server';
 interface createPostProps{
     title:String;
     description:String;
+    info: String;
     status:String;
     poster:String;
+    registrationLink:String;
     socialLink:String;
 }
 
-export async function createPost({title, description, poster, status, socialLink}:createPostProps) {
+export async function createPost({title, description, info, poster, registrationLink, status, socialLink}:createPostProps) {
     console.log(`title=${title}\n description=${description}\n status=${status}\n imgUrl=${poster}\n socialLink=${socialLink}`);
     try {
         const newPost=new Post({
             title:title,
             description:description,
+            info:info,
             status:status,
-            posterUrl:poster,
+            poster:poster,
+            registrationLink:registrationLink,
             socialLink:socialLink,
         });
         await newPost.save();
         const post = await Post.find();
+        console.log("All Posts: ",post);
         return NextResponse.json(post);
     } catch (error) {
         console.log(error);
@@ -31,12 +36,13 @@ export async function createPost({title, description, poster, status, socialLink
 //delete
 //update
 
+//fetching all posts
 export async function allPosts() {
     console.log("All post hit");
     try {
         const posts = await Post.find();
         console.log("All posts in all posts fn: ", posts);
-        return NextResponse.json(posts); // Use NextResponse.json() here
+        return NextResponse.json(posts);
     } catch (error) {
         console.log(error);
         return NextResponse.json({ error: "Failed to fetch posts" }, { status: 500 });
