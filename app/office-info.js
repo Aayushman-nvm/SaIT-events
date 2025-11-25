55da7bccfaa5691042557b3f43167742af09c7e7
Devesh Tiwari <161412073+deveshyaara@users.noreply.github.com>
2025-11-25 16:54:13 +0530

    Correct names of mentors in office-info.js

--- app/office-info.js
+++ app/office-info.js
@@ -6,13 +6,13 @@ const officeInfo = [
         tagline: "Our compass of wisdom, experience, and unwavering support.",
       },
       {
-        NAME: "Dr.Jhon Peter",
-        ABOUT: "Jhon Peter sir is the Head of the Computer Science and Engineering Department and work closely with the student community with respect to tech events.",
+        NAME: "Dr. T John Peter",
+        ABOUT: "John Peter sir is the Head of the Computer Science and Engineering Department and work closely with the student community with respect to tech events.",
         ROLE: "Head of Department, Computer Science and Engineering",
         Image: "",
       },
       {
-        NAME: "Dr.Balakrishnan",
+        NAME: "Dr. K Balakrishnan",
         ABOUT: "Balakrishnan Sir is the go-to person for everything in our community, ensuring smooth execution of all tech events from start to finish.",
         ROLE: "Coordinator-Associate Professor, Computer Science and Engineering",
         Image: "",
